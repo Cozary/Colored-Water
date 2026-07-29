4e2d6c8863d947bd8160829d1a5d340feeefdfd1
Cozary <ayrecio99@gmail.com>
2026-07-29 23:03:32 +0200

    forge crash fixed

---
+++ forge/src/main/java/com/cozary/colored_water/client/ColoredWaterClientForgeEvents.java
@@ -0,0 +1,32 @@
+package com.cozary.colored_water.client;
+
+import com.cozary.colored_water.ColoredWater;
+import com.cozary.colored_water.block.entity.ColoredWaterCauldronBlockEntity;
+import com.cozary.colored_water.init.ModCauldrons;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Handles client-side events that belong to the Forge game bus (not the mod bus).
+ * RegisterColorHandlersEvent does not implement IModBusEvent in Forge 61.0.3,
+ * so it must be subscribed here using Bus.FORGE.
+ */
+@Mod.EventBusSubscriber(modid = ColoredWater.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ColoredWaterClientForgeEvents {
+
+    @SubscribeEvent
+    public static void registerBlockColors(RegisterColorHandlersEvent.Block event) {
+        event.register((state, view, pos, tintIndex) -> {
+            if (view != null && pos != null) {
+                BlockEntity be = view.getBlockEntity(pos);
+                if (be instanceof ColoredWaterCauldronBlockEntity coloredBe) {
+                    return coloredBe.getColor();
+                }
+            }
+            return 0xB43F76E4;
+        }, ModCauldrons.COLORED_WATER_CAULDRON.get());
+    }
+}
